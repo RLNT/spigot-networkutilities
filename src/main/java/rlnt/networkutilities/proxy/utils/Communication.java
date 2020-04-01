c897c83cac8892688af3594a0d0b6a16892b54c0
Relentless <damnrelentless.yt@gmail.com>
2020-04-02 00:56:37 +0200

    Removed unnecessary import
    Signed-off-by: DAmNRelentless <damnrelentless.yt@gmail.com>

--- src/main/java/rlnt/networkutilities/proxy/utils/Communication.java
+++ src/main/java/rlnt/networkutilities/proxy/utils/Communication.java
@@ -5,7 +5,6 @@ import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.config.Configuration;
-import org.bukkit.command.Command;
 
 import java.util.Collection;
 import java.util.Map;
