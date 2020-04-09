e0a8b1ddca34ffc1f6f5bcd48edf5a767c7be164
Relentless <damnrelentless.yt@gmail.com>
2020-04-09 08:17:18 +0200

    Revert "Fixed null pointer exception when whitelist wasn't loaded yet"
    This reverts commit 8af225f2e0c589f7346932ab03d1e8421ba91d1f.

--- src/main/java/rlnt/networkutilities/proxy/NetworkUtilities.java
+++ src/main/java/rlnt/networkutilities/proxy/NetworkUtilities.java
@@ -71,7 +71,7 @@ public class NetworkUtilities extends Plugin {
         getProxy().unregisterChannel("networkutilities");
 
         try {
-            if (whitelist != null) Whitelist.save();
+            Whitelist.save();
         } catch (PluginConfigException e) {
             e.printStackTrace();
         }
